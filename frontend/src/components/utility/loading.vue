96b5f4a2425b89ffcfe2b23c213cc04033437079
aseiler_mobile <e11771276@student.tuwien.ac.at>
2022-01-24 20:48:14 +0100

    adding loading component

---
+++ frontend/src/components/utility/loading.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-overlay :value="loading">
+        <v-progress-circular indeterminate></v-progress-circular>
+    </v-overlay>
+</template>
+
+<script>
+export default {
+    name: 'Loading',
+    props: ['loading']
+}
+</script>
