8055a774f881760ca19ab3017b973a30dfce0167
aseiler_mobile <e11771276@student.tuwien.ac.at>
2022-01-24 20:46:51 +0100

    removing logging line

--- backend/src/hooks/logger.hook.js
+++ backend/src/hooks/logger.hook.js
@@ -2,6 +2,5 @@ const logger = require('../logger')
 
 module.exports = async context => {
     logger.info(`[${context.method} ${context.path}]`)
-    logger.info(`params: ${JSON.stringify(context.params)}`)
     return context
 }
