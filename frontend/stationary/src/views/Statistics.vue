4ac9841fea2940892c67b0841456eba4477d8c09
ASeiler <e11771276@student.tuwien.ac.at>
2023-01-19 17:17:25 +0100

    including date and timepicker for timeframe selection of chart and adding download-features for csv and json

--- frontend/stationary/src/views/Statistics.vue
+++ frontend/stationary/src/views/Statistics.vue
@@ -8,13 +8,14 @@
               Filters
             </v-expansion-panel-header>
             <v-expansion-panel-content>
-              <v-row>
+              <v-row class="my-4">
                 <v-col cols="4">
-                  <v-select label="Stationsname" v-model="selectedStation" :items="stations" outlined></v-select>
+                  <v-select label="Stationsname" v-model="selectedStation" :items="stations" outlined
+                    hide-details></v-select>
                 </v-col>
                 <v-col cols="4">
                   <v-select label="Kategorien" v-model="selectedCategories" :items="categories" outlined
-                    item-text="name" item-value="id" multiple>
+                    item-text="name" item-value="id" multiple hide-details>
                     <template v-slot:selection="{ item, index }">
                       <v-chip v-if="index < 1">
                         <span>{{ item.name }}</span>
@@ -26,7 +27,7 @@
                 </v-col>
                 <v-col cols="4">
                   <v-select label="Items" multiple v-model="selectedItems" :items="modifiedItems" item-text="name"
-                    item-value="id" outlined>
+                    item-value="id" outlined hide-details>
                     <template v-slot:selection="{ item, index }">
                       <v-chip v-if="index < 1">
                         <span>{{ item.name }}</span>
@@ -38,24 +39,55 @@
                   </v-select>
                 </v-col>
               </v-row>
-              <v-row>
-                <v-col cols="3"></v-col>
-                <v-col cols="3"></v-col>
-                <v-col cols="3"></v-col>
-                <v-col cols="3"></v-col>
+              <v-divider></v-divider>
+              <v-row class="my-4">
+                <v-col cols="3">
+                  <DatePicker label="Start-Datum" v-model="startDate" />
+                </v-col>
+                <v-col cols="3">
+                  <TimePicker label="Start-Zeit" v-model="startTime" />
+                </v-col>
+                <v-divider vertical></v-divider>
+                <v-col cols="3">
+                  <DatePicker label="End-Datum" v-model="endDate" />
+                </v-col>
+                <v-col cols="3">
+                  <TimePicker label="End-Zeit" v-model="endTime" />
+                </v-col>
               </v-row>
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
       </v-col>
       <v-col cols="12">
-        <LineChartGenerator :chart-options="chartOptions" :chart-data="clusteredOrderedItems" dataset-id-key="label" :width="500"
-          :plugins="[]" :css-classes="''" :styles="{}" :chart-id="'LineChart'" :height="400" />
+        <LineChartGenerator :chart-options="chartOptions" :chart-data="clusteredOrderedItems" dataset-id-key="label"
+          :width="500" :plugins="[]" :css-classes="''" :styles="{}" :chart-id="'LineChart'" :height="400" />
       </v-col>
     </v-row>
+    <v-speed-dial v-model="fab" direction="top" open-on-hover transition="slide-y-reverse-transition" elevation="2"
+      bottom right fixed style="right: 45px; bottom: 55px;">
+      <template v-slot:activator>
+        <v-btn v-model="fab" fab fixed bottom right>
+          <v-icon v-if="fab">
+            close
+          </v-icon>
+          <v-icon v-else>
+            download
+          </v-icon>
+        </v-btn>
+      </template>
+      <v-btn fab dark small color="green" @click="exportJson">
+        {}
+      </v-btn>
+      <v-btn fab dark small color="indigo" @click="exportCsv">
+        .csv
+      </v-btn>
+    </v-speed-dial>
   </v-container>
 </template>
 <script>
+import TimePicker from '../components/utilities/TimePicker.vue'
+import DatePicker from '../components/utilities/DatePicker.vue'
 import { mapActions, mapGetters } from 'vuex'
 import moment from 'moment'
 import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
@@ -84,7 +116,9 @@ export default {
   name: 'Statistics',
   props: [],
   components: {
-    LineChartGenerator
+    LineChartGenerator,
+    TimePicker,
+    DatePicker
   },
   data() {
     return {
@@ -107,7 +141,12 @@ export default {
       chartOptions: {
         responsive: true,
         maintainAspectRatio: false
-      }
+      },
+      startDate: null,
+      startTime: null,
+      endDate: null,
+      endTime: null,
+      fab: null
     }
   },
   created: function () { },
@@ -148,6 +187,73 @@ export default {
       let baseItems = this.findBaseItems({ query: { categoryId } }).data
       let items = this.findItems({ query: { baseItemId: { $in: baseItems.map(({ id }) => id) } } }).data
       this.selectedItems = [...this.selectedItems.filter(itemId => !items.map(({ id }) => id).includes(itemId))]
+    },
+    downloadFile: function (filename = '', content = '') {
+      let element = document.createElement('a')
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content))
+      element.setAttribute('download', filename)
+      element.style.display = 'none'
+      document.body.appendChild(element)
+      element.click()
+      document.body.removeChild(element)
+    },
+    exportJson: function () {
+      if (!this.startDate || !this.startTime || !this.endDate || !this.endTime) {
+        return
+      }
+      let startTimestamp = moment(`${this.startDate}T${this.startTime}`)
+      let endTimestamp = moment(`${this.endDate}T${this.endTime}`)
+      let hourDifference = Math.ceil(endTimestamp.diff(startTimestamp, 'hours'))
+      endTimestamp = moment(startTimestamp).add(hourDifference, 'hours')
+      let data = this.filteredOrderedItems
+        .filter(({ createdAt }) => {
+          let created = moment.utc(createdAt)
+          return created.isBetween(startTimestamp, endTimestamp, null, '[]')
+        })
+        .map(orderedItem => {
+          let item = this.getItem(orderedItem.itemId)
+          return {
+            ...orderedItem,
+            name: `${this.getSize(item.sizeId).name} ${this.getBaseItem(item.baseItemId).name}, ${this.getFlavour(item.flavourId).name}`
+          }
+        })
+      if (data.length === 0) {
+        console.log('Nothing to download!')
+        return
+      }
+      let stringifiedData = JSON.stringify(data)
+      this.downloadFile(`${startTimestamp.format()}_${endTimestamp.format()}_${this.selectedStation}.json`, stringifiedData)
+    },
+    exportCsv: function () {
+      if (!this.startDate || !this.startTime || !this.endDate || !this.endTime) {
+        return
+      }
+      let startTimestamp = moment(`${this.startDate}T${this.startTime}`)
+      let endTimestamp = moment(`${this.endDate}T${this.endTime}`)
+      let hourDifference = Math.ceil(endTimestamp.diff(startTimestamp, 'hours'))
+      endTimestamp = moment(startTimestamp).add(hourDifference, 'hours')
+      let data = this.filteredOrderedItems
+        .filter(({ createdAt }) => {
+          let created = moment.utc(createdAt)
+          return created.isBetween(startTimestamp, endTimestamp, null, '[]')
+        })
+        .map(orderedItem => {
+          let item = this.getItem(orderedItem.itemId)
+          return {
+            ...orderedItem,
+            name: `${this.getSize(item.sizeId).name} ${this.getBaseItem(item.baseItemId).name}, ${this.getFlavour(item.flavourId).name}`
+          }
+        })
+      if (data.length === 0) {
+        console.log('Nothing to download!')
+        return
+      }
+      let csvContentArray = ['Date;Time;Quantity;Item;Price;', ...data.map(({ createdAt, itemId, quantity }) => {
+        let item = this.getItem(itemId)
+        let itemName = `${this.getSize(item.sizeId).name} ${this.getBaseItem(item.baseItemId).name}, ${this.getFlavour(item.flavourId).name}`
+        return `${moment(createdAt).format('YYYY-MM-DD')};${moment(createdAt).format('HH:mm:ss')};${quantity};${itemName};${item.price};`
+      }), '']
+      this.downloadFile(`${startTimestamp.format()}_${endTimestamp.format()}_${this.selectedStation}.csv`, csvContentArray.join('\r\n'))
     }
   },
   watch: {
@@ -198,12 +304,19 @@ export default {
         }
       })
     },
+    filteredOrderedItems: function () {
+      return this.findOrderedItems({ query: { waiter: this.selectedStation, itemId: { $in: this.selectedItems } } }).data
+    },
     clusteredOrderedItems: function () {
-      let now = moment()
-      now.subtract(24, 'hours')
-      let timeFrames = Array(24).fill(0).map(() => {
-        let start = now.format()
-        let end = now.add(1, 'hour').format()
+      if (!this.startDate || !this.startTime || !this.endDate || !this.endTime) {
+        return { labels: [], datasets: [] }
+      }
+      let startTimestamp = moment(`${this.startDate}T${this.startTime}`)
+      let endTimestamp = moment(`${this.endDate}T${this.endTime}`)
+      let hourDifference = Math.ceil(endTimestamp.diff(startTimestamp, 'hours'))
+      let timeFrames = Array(Math.abs(hourDifference + 1)).fill(0).map(() => {
+        let start = startTimestamp.format()
+        let end = startTimestamp.add(1, 'hour').format()
         return { start, end }
       })
       let labels = timeFrames.map(({ start, end }) => {
@@ -216,8 +329,7 @@ export default {
         let backgroundColor = this.colors[index % this.colors.length]
         let borderColor = this.colors[index % this.colors.length]
         let data = timeFrames.map(({ start, end }) => {
-          return this.orderedItems
-            .filter(({ waiter }) => waiter === this.selectedStation)
+          return this.filteredOrderedItems
             .filter(({ itemId: oiId }) => oiId === itemId)
             .filter(({ createdAt }) => {
               let created = moment.utc(createdAt)
